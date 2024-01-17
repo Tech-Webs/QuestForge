8afc232106d2117e584fb271fe3d33921c959a58
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-01-16 23:53:27 -0300

    Logo update

--- questforge/src/components/Navbar.js
+++ questforge/src/components/Navbar.js
@@ -2,15 +2,18 @@ import styles from './Navbar.module.css'
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/LOGO_FODASTICA_POLICIA.png'
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <header>
             <nav>
-                <a className={styles.logoImg}><img src={logo} alt='Logo_QuestForge'/></a>
-                <a className = {styles.Logo} href ="/">QuestForge</a>
-                <ul className = {styles.navList}>
-                    <li><a href = "/">Sobre</a></li>
-                    <li><a  href = ""><Link to ="/perfil">Perfil</Link></a></li>
+                <div className='LogoCompleta'>
+                    <a href='/'><img className={styles.LogoImg} src={logo} alt='Logo_QuestForge' /></a>
+                    <a className={styles.Logo} href="/">QuestForge</a>
+                </div>
+
+                <ul className={styles.navList}>
+                    <li><a href="/">Sobre</a></li>
+                    <li><a href=""><Link to="/perfil">Perfil</Link></a></li>
                 </ul>
             </nav>
         </header>
