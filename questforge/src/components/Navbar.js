db67d47655f42af4b4663450552e7258d0c3dbef
Eduardo Araújo <127903788+EduardoFA45@users.noreply.github.com>
2024-01-17 00:44:30 -0300

    att dudu

--- questforge/src/components/Navbar.js
+++ questforge/src/components/Navbar.js
@@ -5,17 +5,17 @@ import logo from '../assets/images/LOGO_FODASTICA_POLICIA.png'
 function Navbar() {
     return (
         <header>
-            <nav>
-                <div className='LogoCompleta'>
-                    <a href='/'><img className={styles.LogoImg} src={logo} alt='Logo_QuestForge' /></a>
-                    <a className={styles.Logo} href="/">QuestForge</a>
-                </div>
-
-                <ul className={styles.navList}>
-                    <li><a href="/">Sobre</a></li>
-                    <li><a href=""><Link to="/perfil">Perfil</Link></a></li>
-                </ul>
-            </nav>
+            <section className='navbar'>
+                <nav>
+                        <a href='/'><img className={styles.LogoImg} src={logo} alt='Logo_QuestForge' /></a>
+                        <a className={styles.Name} href="/">QuestForge</a>
+                        
+                    <ul className={styles.navList}>
+                        <li><a href="/">Sobre</a></li>
+                        <li><a href=""><Link to="/perfil">Perfil</Link></a></li>
+                    </ul>
+                </nav>
+            </section>
         </header>
     )
 }
